8b7260956a26f0f760c5bdb0d95be2a4cacf83bf
Corentin.Vives <corentin.vives@insa-rennes.fr>
2021-10-25 11:35:27 +0200

    [feat] ProductController + Product: ProduitGratuitException used

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -70,6 +71,9 @@ public class ProductController {
     @PostMapping(value = "/Produits")
 
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
+        if (product.getPrix() == 0) {
+            throw new ProduitGratuitException("Un produit ne peut pas être gratuit");
+        }
 
         Product productAdded =  productDao.save(product);
 
